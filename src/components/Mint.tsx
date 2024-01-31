255962b3e76a1c1b5c7fbca1b4dbffc8958b9986
Kritik <kritiksah0@gmail.com>
2024-02-01 00:51:42 +0530

    Floor symbol fix

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -103,7 +103,10 @@ const MintNft = () => {
       options
     )
       .then((response) => response.json())
-      .then((response) => setCollectionStats(response))
+      .then((response) => {
+        console.log(response);
+        setCollectionStats(response);
+      })
       .catch((err) => console.error(err));
   }, []);
 
@@ -175,7 +178,7 @@ const MintNft = () => {
             <div className="flex h-20 min-w-40 flex-col items-center justify-center rounded-md border border-dashed border-ui-white/10 transition-colors duration-100 ease-in-out hover:border-gray-400/80">
               <div className="flex flex-row items-center justify-center">
                 <span className="font-bold text-ui-highlight">
-                  {collectionStats?.total?.floor_price}
+                  {`${collectionStats?.total?.floor_price} ${collectionStats?.total?.floor_price_symbol}`}
                 </span>
               </div>
               <div className="mt-2 text-sm text-gray-400">Floor Price</div>
